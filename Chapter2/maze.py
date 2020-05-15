ac0746eed8153ebee1bd3bdc1727aff2b6cb5bac
Karuka3 <karuka.piplup.0327@gmail.com>
2020-05-15 21:27:55 +0900

    Change import order

--- Chapter2/maze.py
+++ Chapter2/maze.py
@@ -1,6 +1,6 @@
+import random
 from enum import Enum
 from typing import List, NamedTuple, Callable, Optional
-import random
 from math import sqrt
 from generic_search import dfs, bfs, astar, node_to_path, Node
 
